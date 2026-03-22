8fc5285ccdc757470182a4f5fcea125af2f21e29
yaoman <xueguangyue@gmail.com>
2026-03-22 16:19:08 +0800

    Full Sync: 2026-03-22 16:19 (Automated Upload)

--- C_proj/C_proj/main.c
+++ C_proj/C_proj/main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-int main() {
-int birth_year = 1980;
-printf("\n   请输入你的年纪   \n"）
-int current_age;
-scanf("%d", &current_age);
-int calculated_year = 2026 - current_age;
-printf("\n--- 时光机扫描结果 ---\n");
-printf("I Think Your Age is: %d\n",calculated_year);
-if (calculated_year == birth_year){
-    printf("yes,you r a 1980's solider! \n");
-}else {
-    printf("note:r u sure? \n");
-}
+
+int main(){
+    int gold = 999;
+    int *p;
+    p = &gold;
+    printf("变量gold的值是：%d\n",gold);
+    printf("变量gold的内存地址是：%p\n",&gold);
+    printf("指针p记录的内容是：%p\n",p);
+    printf("通过指针p钻进去看的值是：%d\n",*p);
     return 0;
 }
