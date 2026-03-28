f188e7ebc3060c076e4209c4ed8fce3b4620d4be
yaoman <xueguangyue@gmail.com>
2026-03-28 14:15:37 +0800

    Full Sync: 2026-03-28 14:15 (Automated Upload)

---
+++ CCC/CCC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CCC
+//
+//  Created by guangyao xue on 2026/3/28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
