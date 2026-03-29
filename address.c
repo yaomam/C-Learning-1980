b963efc6d28d88416786e943edb4c6835c6fdbe0
yaoman <49480627+yaomam@users.noreply.github.com>
2026-03-29 19:07:03 +0800

    Full Sync: 2026-03-29 19:07 (Automated Upload)

--- address.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int year = 1980;
-    printf("数据内容: %d\n", year);
-    printf("它的内存地址: %p\n", &year);
-    return 0;
-}
--- test.c
+++
@@ -1,5 +0,0 @@
-int main() {
-    int gold = 1000;
-    gold = gold + 1;
-    return 0;
-}
